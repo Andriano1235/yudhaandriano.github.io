079e7cbd6e3191608c135d4389eb473a927d6d43
Andriano1235 <yudhaandriano32@gmail.com>
2026-05-07 02:38:49 +0700

    fixed bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+// import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
@@ -8,7 +8,7 @@ import rehypeKatex from "rehype-katex";
 // https://astro.build/config
 export default defineConfig({
   // site: "https://astrofy-template.netlify.app",
-  site: 'https://Andriano1235.github.io',
+  site: 'https://yudhaandriano.github.io',
 
   integrations: [
     mdx(),
