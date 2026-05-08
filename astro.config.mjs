0864a7e98dd1e86bb9e37645c2c4719b2446f649
Andriano1235 <yudhaandriano32@gmail.com>
2026-05-08 08:58:43 +0700

    fixed bug sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-// import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
@@ -12,7 +12,7 @@ export default defineConfig({
 
   integrations: [
     mdx(),
-    // sitemap(),
+    sitemap(),
     tailwind(),
   ],
 
